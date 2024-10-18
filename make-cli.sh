85f6e9793a20c27fdacb6894661cd0336fdaeb39
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-18 11:12:29 -0400

    Create make-cli.sh

---
+++ make-cli.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+umask 022
+_tmp_dir="$(mktemp -d)"
+cd "${_tmp_dir}"
+#wget -c -t 9 -T 9 "https://github.com/moby/moby/archive/refs/tags/v23.0.15.tar.gz"
+#tar -xof v*.tar*
+#sleep 1
+#rm -vf v*.tar*
+#cd moby*
+
+git clone https://github.com/docker/cli.git
+cd cli
+git checkout v26.1.5
+
+#make all
+make binary
+echo
+echo
+make plugins
+echo
+echo ' done '
+echo
+echo
+exit
+
