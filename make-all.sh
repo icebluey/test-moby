956091e0b3faecaaae47fa1fdd0497029da01a1d
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-18 07:47:01 -0400

    Update make-all.sh

--- make-all.sh
+++ make-all.sh
@@ -4,12 +4,14 @@ TZ='UTC'; export TZ
 umask 022
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
-wget -c -t 9 -T 9 "https://github.com/moby/moby/archive/refs/tags/v23.0.15.tar.gz"
-tar -xof v*.tar*
-sleep 1
-rm -vf v*.tar*
-cd moby*
+#wget -c -t 9 -T 9 "https://github.com/moby/moby/archive/refs/tags/v23.0.15.tar.gz"
+#tar -xof v*.tar*
+#sleep 1
+#rm -vf v*.tar*
+#cd moby*
+
+https://github.com/moby/moby.git
+cd moby
+git checkout v26.1.5
 make all
 exit
-
-
